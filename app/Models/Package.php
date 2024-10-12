44f35017dfc3aa2e9e072de05a540f95c38aa885
Yaang Vu <yaangvu@gmail.com>
2024-10-12 15:29:54 +0700

    Add package barryvdh/laravel-ide-helper

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -2,11 +2,46 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
 
+/**
+ *
+ *
+ * @property int $id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property string $code
+ * @property string $title
+ * @property string $price
+ * @property string|null $currency
+ * @property string|null $type
+ * @property int $user_id
+ * @property int $game_id
+ * @property-read Game $game
+ * @property-read Collection<int, Token> $tokens
+ * @property-read int|null $tokens_count
+ * @property-read User $user
+ * @method static Builder|Package newModelQuery()
+ * @method static Builder|Package newQuery()
+ * @method static Builder|Package query()
+ * @method static Builder|Package whereCode($value)
+ * @method static Builder|Package whereCreatedAt($value)
+ * @method static Builder|Package whereCurrency($value)
+ * @method static Builder|Package whereGameId($value)
+ * @method static Builder|Package whereId($value)
+ * @method static Builder|Package wherePrice($value)
+ * @method static Builder|Package whereTitle($value)
+ * @method static Builder|Package whereType($value)
+ * @method static Builder|Package whereUpdatedAt($value)
+ * @method static Builder|Package whereUserId($value)
+ * @mixin Eloquent
+ */
 class Package extends Model
 {
     protected $fillable = ['price', 'code', 'user_id', 'game_id', 'title', 'currency', 'type'];
@@ -24,5 +59,5 @@ class Package extends Model
     public function tokens(): HasMany
     {
         return $this->hasMany(Token::class);
-    }  use HasFactory;
+    }
 }
--- app/Models/Token.php
+++ app/Models/Token.php
@@ -2,10 +2,44 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
+/**
+ *
+ *
+ * @property int $id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property string $token
+ * @property string|null $original_json
+ * @property string|null $signature
+ * @property string|null $order_id
+ * @property int $user_id
+ * @property int $game_id
+ * @property int $package_id
+ * @property-read Game $game
+ * @property-read Package $package
+ * @property-read User $user
+ * @method static Builder|Token newModelQuery()
+ * @method static Builder|Token newQuery()
+ * @method static Builder|Token query()
+ * @method static Builder|Token whereCreatedAt($value)
+ * @method static Builder|Token whereGameId($value)
+ * @method static Builder|Token whereId($value)
+ * @method static Builder|Token whereOrderId($value)
+ * @method static Builder|Token whereOriginalJson($value)
+ * @method static Builder|Token wherePackageId($value)
+ * @method static Builder|Token whereSignature($value)
+ * @method static Builder|Token whereToken($value)
+ * @method static Builder|Token whereUpdatedAt($value)
+ * @method static Builder|Token whereUserId($value)
+ * @mixin Eloquent
+ */
 class Token extends Model
 {
     protected $fillable = ['token', 'user_id', 'game_id', 'package_id', 'original_json', 'signature', 'order_id'];
